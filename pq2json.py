d2fea85caa3cb4fcd0a3e0e25f3ec7a9417451aa
noah888999666 <noah888999666@gmail.com>
2025-07-23 19:05:20 +0800

    Update pq2json.py

--- pq2json.py
+++ pq2json.py
@@ -1,1 +1,62 @@
-.....
+[
+  {
+    "step": 30,
+    "input_text": "",
+    "output_text": "",
+    "score": 0.1367992461,
+    "reward_model": {
+      "candidate_answers": "[\"Kingdom of Spain\", \"ESP\", \"\\ud83c\\uddea\\ud83c\\uddf8\", \"ES\", \"Spain\"]",
+      "ground_truth": "Spain",
+      "style": "rule"
+    },
+    "image_url": null
+  },
+  {
+    "step": 30,
+    "input_text": "",
+    "output_text": "",
+    "score": 0.1367992461,
+    "reward_model": {
+      "candidate_answers": "[\"Trikay\", \"Padmayani\", \"Trigyesh\", \"Mahatma\", \"Siddarth\", \"Siddh\\u0101rtha Gautama\", \"Gautam\", \"Fo\", \"Buddhadeva\", \"Shakyamuni\", \"Tatharaj\", \"Trigya\", \"Lokpradeep\", \"Buddha\", \"Khajit\", \"Munish\", \"Shakyasinha\", \"Lord Buddha\", \"Tathagat\", \"Sakyasinha\", \"Shaakya\", \"Sakyamuni\", \"Prabudha\", \"Sakya-muni\", \"Saugata\", \"Gotam\", \"Devarajalu\", \"Gautama Buddha\"]",
+      "ground_truth": "Gautama Buddha",
+      "style": "rule"
+    },
+    "image_url": null
+  },
+  {
+    "step": 30,
+    "input_text": "",
+    "output_text": "",
+    "score": 0.1367992461,
+    "reward_model": {
+      "candidate_answers": "[\"Order of Saint John of Jerusalem\", \"Order of Saint John\", \"O.S.Io.Hieros.\", \"Hospitallers\", \"Order of the Hospital of St. John of Jerusalem\", \"Order of Hospitallers\", \"Knights of Saint John\", \"Knights of St. John\"]",
+      "ground_truth": "Order of Hospitallers",
+      "style": "rule"
+    },
+    "image_url": null
+  },
+  {
+    "step": 30,
+    "input_text": "",
+    "output_text": "",
+    "score": 0.1367992461,
+    "reward_model": {
+      "candidate_answers": "[\"Tuileries Palace\", \"Palais des Tuileries\"]",
+      "ground_truth": "Tuileries Palace",
+      "style": "rule"
+    },
+    "image_url": null
+  },
+  {
+    "step": 30,
+    "input_text": "",
+    "output_text": "",
+    "score": 0.1367992461,
+    "reward_model": {
+      "candidate_answers": "[\"PRT\", \"Portugal\", \"\\ud83c\\uddf5\\ud83c\\uddf9\", \"Rep\\u00fablica Portuguesa\", \"PT\", \"Portuguese Republic\"]",
+      "ground_truth": "Portugal",
+      "style": "rule"
+    },
+    "image_url": null
+  }
+]
